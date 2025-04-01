b0c1668267772251fc87ee8e99a97c08cf10b007
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-04-01 14:02:27 +0530

    changes

--- client/src/app/leaderboard/page.tsx
+++ client/src/app/leaderboard/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import API from "../../utils/api";
+import { apiFetch } from "../../utils/api";
 
 export default function Leaderboard() {
   const [leaders, setLeaders] = useState([]);
 
   useEffect(() => {
     async function fetchLeaderboard() {
-      const { data } = await API.get("/trade/leaderboard");
+      const { data } = await apiFetch.get("/trade/leaderboard");
       setLeaders(data);
     }
     fetchLeaderboard();
@@ -18,7 +18,7 @@ export default function Leaderboard() {
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Leaderboard</h1>
       <ul>
-        {leaders.map((user: any, index) => (
+        {leaders.map((user: any, index: any) => (
           <li key={index} className="border p-2 rounded mb-2">
             {index + 1}. {user.name} - ₹{user.portfolioValue}
           </li>
